ef4457da2031301b5377fbb56084cfafc990156e
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-11-03 22:11:30 +0000

    current file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """json file"""
-import models
+
 import json
+import uuid
+import os
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -13,34 +15,33 @@ from models.amenity import Amenity
 class FileStorage():
     """class"""
     __file_path = 'file.json'
-    self.__objects = {}
+    __objects = {}
 
     def all(self):
         """return dictionary"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """obj class name id in object"""
-        new = obj.class.__name__ + "." + obj.id
-        self.__object[new] = obj
+        FileStorage.__objects[obj.__class__.__name__ + "." + str(obj.id)] = obj
 
 
     def save(self):
         """save to objct"""
         nvd = {}
         with open(FileStorage.__file_path, 'w') as json:
-            for key, value in self.__object,items():
+            for key, value in FileStorage.__object,items():
                 nvd[key] = value.to_dict()
             filejson.write(json.dumps(nvd))
 
     def reload(self):
         """reload json in the file object"""
         try:
-            with open(fileStorage.__file_path, 'r') as json_r:
+            with open(FileStorage.__file_path, 'r') as json_r:
                 rj = json.loads(json_r.read())
                 for key, value in rj.item():
                     keysl = key.split(".")
                     nvd = globals()[keysl[0]](**value)
-                    self.__object[key] = copy
+                    FileStorage.__object[key] = nvd
         except IOError:
             return
