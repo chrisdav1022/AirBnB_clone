0cc470b4f62e4d0482ecb47777dc80ecf750d766
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-11-03 18:57:19 +0000

    current file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,14 @@
-#!/usr/bin/puthon3
+#!/usr/bin/python3
 """json file"""
 import models
 import json
 from models.base_model import BaseModel
-from models.state import state
+from models.user import User
+from models.state import State
+from models.review import Review
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
 
 class FileStorage():
     """class"""
