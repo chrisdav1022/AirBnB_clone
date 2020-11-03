698c64c9fbe9d9549876d929e596765119244aba
ChristianCampos-55 <1566@holbertonschool.com>
2020-11-03 17:27:59 +0000

    [ADD] Python code for project objs to use

--- models/base_model.py
+++ models/base_model.py
@@ -1,38 +1,50 @@
 #!/usr/bin/python3
-""" Base Model Module """
-import models
-from uuid import uuid4
+""" Class BaseModel """
 from datetime import datetime
+from uuid import uuid4
+import models
+
 
 class BaseModel:
-    """A BaseModel class"""
+    """ construct """
 
     def __init__(self, *args, **kwargs):
-        """Initialize class base"""
+        """ Construct """
         if kwargs:
-            for keys, v in kwargs.items():
-                if keys != "__class__":
-                    if keys == "created_at" or keys == "updated_at":
-                        v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, keys, v)
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'created_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if 'id' not in kwargs.keys():
+                    self.id = str(uuid4())
+                if 'created_at' not in kwargs.keys():
+                    self.created_at = datetime.now()
+                if 'updated_at' not in kwargs.keys():
+                    self.updated_at = datetime.now()
+                setattr(self, key, value)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
-        """print the class name, id and directory"""
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                      self.id, self.__dict__))
+        """ String """
+        return('[' + type(self).__name__ + '] (' + str(self.id) +
+               ') ' + str(self.__dict__))
 
     def save(self):
-        """updates the public instance attribute with the current datetime"""
+        """ save function """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary """
-        new = self.__dict__.copy()
-        new["__class__"] = self.__class__.__name__
-        new["created_at"] = self.created_at.isoformat()
-        new["updated_at"] = self.updated_at.isoformat()
-        return new
+        """ Return a dictonary """
+        aux_dict = self.__dict__.copy()
+        aux_dict['__class__'] = self.__class__.__name__
+        aux_dict['created_at'] = self.created_at.isoformat()
+        aux_dict['updated_at'] = self.updated_at.isoformat()
+        return aux_dict
