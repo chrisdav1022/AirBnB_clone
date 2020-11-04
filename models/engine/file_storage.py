a9207039f7e3be00d87fcb72382023e5ed57c1d6
ChristianCampos-55 <1566@holbertonschool.com>
2020-11-04 02:29:45 +0000

    [RFACT] File storage general refactor

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,47 +1,45 @@
 #!/usr/bin/python3
-"""json file"""
-
+""" class FileStorage
+    serializes instances to a JSON file
+    and deserializes JSON file to instances """
 import json
 import uuid
 import os
+from datetime import datetime
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
-from models.review import Review
-from models.place import Place
 from models.city import City
 from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
-class FileStorage():
-    """class"""
-    __file_path = 'file.json'
+
+class FileStorage:
+    """ construct """
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """return dictionary"""
+        """ return dictionary objects """
         return FileStorage.__objects
 
     def new(self, obj):
-        """obj class name id in object"""
+        """ sets in dictionary the obj with key <obj class name>.id """
         FileStorage.__objects[obj.__class__.__name__ + "." + str(obj.id)] = obj
 
-
     def save(self):
-        """save to objct"""
-        nvd = {}
-        with open(FileStorage.__file_path, 'w') as json:
-            for key, value in FileStorage.__object,items():
-                nvd[key] = value.to_dict()
-            filejson.write(json.dumps(nvd))
+        """ serializes objectss to the JSON file (path: __file_path) """
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as fname:
+            new_dict = {key: obj.to_dict() for key, obj in
+                        FileStorage.__objects.items()}
+            json.dump(new_dict, fname)
 
     def reload(self):
-        """reload json in the file object"""
-        try:
-            with open(FileStorage.__file_path, 'r') as json_r:
-                rj = json.loads(json_r.read())
-                for key, value in rj.item():
-                    keysl = key.split(".")
-                    nvd = globals()[keysl[0]](**value)
-                    FileStorage.__object[key] = nvd
-        except IOError:
-            return
+        """ Reload the file """
+        if (os.path.isfile(FileStorage.__file_path)):
+            with open(FileStorage.__file_path, 'r', encoding="utf-8") as fname:
+                l_json = json.load(fname)
+                for key, val in l_json.items():
+                    FileStorage.__objects[key] = eval(
+                        val['__class__'])(**val)
